5510db7848377b77247a4baa5152152401cf68a1
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 21:50:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,10 @@
       dodger.style.left = `${left - 1}px`;
     }
   }
-  function moveDodgerRight(){
-    let RightNumbers = dodger.style.left.replace("px", "");
-    let Right = parseInt(RightNumbers, 10);
-
-    if (Right < 0) {
-      dodger.style.left = `${Right - 1}px`;
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
     }
   }
